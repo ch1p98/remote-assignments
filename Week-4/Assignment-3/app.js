82313e1cf6061576052ef969d6cdc357bf50d7e1
ch1p98 <alexpalladium@gmail.com>
2022-07-05 18:00:17 +0800

    W4A3 first commit

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,14 +1,76 @@
 const express = require("express");
 const body_parser = require("body-parser");
 const cookie_parser = require("cookie-parser");
-
+const mysql = require("mysql");
 const app = express();
+
+const db = mysql.createConnection({
+  host: "54.71.133.152",
+  user: "root",
+  password: "test^8test",
+  database: "assignment",
+});
+//connection
+/*db.connect()
+  .then(() => {
+    console.log("connected to server.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });*/
+
+//tutorial edition
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("connected to server.");
+});
+
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(cookie_parser());
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+app.get("/signup", (req, res) => {
+  res.render("./signup");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("./signup");
+});
+
+app.post("/signin", (req, res) => {});
+
+app.post("/signup", (req, res) => {});
 
 app.get("/", (req, res) => {
-  let name = "Alex";
-  res.send("Hello");
+  const user_id = req.cookies.user_id;
+  console.log(user_id);
+  if (user_id) {
+    res.render("index", { user_id });
+  } else {
+    res.redirect("/signup");
+  }
+});
+
+app.get("/create_db", (req, res) => {
+  const sql = "CREATE DATABASE assignment;";
+  db.query(sql, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    res.send(`database "${sql.split(" ")[2].split(";")[0]}" created.`);
+  });
+});
+
+app.get("/create_table", (req, res) => {
+  const sql =
+    "CREATE TABLE users(id int AUTO_INCREMENT, email VARCHAR(255), password VARCHAR(255), PRIMARY KEY (id))";
+  db.query(sql, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    res.send(`table "${sql.split(" ")[2]}" created.`);
+  });
 });
 
 app.listen(3000, () => {
